5b2bb538fca64db6e9a1001dcc07066a5507b7d8
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2024-12-19 10:46:59 +0100

    Added space at the end of the line

--- lab05:find-command/code/more_recent.sh
+++ lab05:find-command/code/more_recent.sh
@@ -5,14 +5,14 @@ FILES=`find /usr/include/linux/ -mindepth 2 -name "*.h" -print`
 if [[ -z ${FILES} ]] ; then
     echo "nothig found"
 else
-    for RECENTE in ${FILES} ; do # assegno il primo
+    for RECENT in ${FILES} ; do # assegno il primo
         break
     done
     for NAME in ${FILES} ; do
-        if [[ ${RECENTE} -ot ${NAME} ]] ; then
-            RECENTE=${NAME}
+        if [[ ${RECENT} -ot ${NAME} ]] ; then
+            RECENT=${NAME}
         fi
     done
 fi
 
-echo "il file piu' recente e' ${RECENTE}"
+echo "il file piu' recente e' ${RECENT}"
